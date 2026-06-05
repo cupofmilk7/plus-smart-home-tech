497b849c554d25079a8a593f6b91f873e6ce65a8
Fedor Antonenko <cupofmilk80@gmail.com>
2026-06-05 14:30:13 +0300

    1) создание и конфигурация gateway

--- infra/gateway/src/main/java/ru/yandex/practicum/Gateway.java
+++ infra/gateway/src/main/java/ru/yandex/practicum/Gateway.java
@@ -3,14 +3,11 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ConfigServer {
-
+public class Gateway {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServer.class, args);
+        SpringApplication.run(Gateway.class, args);
     }
 }
